b610949349b3acac1adbcb97f595522f4195f198
Rebecca Oskarsson <Rebecca@Rebeccas-MacBook-Pro.local>
2014-10-29 17:37:45 +0100

    Light.java pretty much done

--- Light.java
+++ Light.java
@@ -3,16 +3,41 @@ public class Light {
     private int time;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
     private int green; // Signalen grön när time<green 
 
-    public Light(int period, int green) {...}
+    public Light(int period, int green) {
+	this.period = period;
+	this.green  = green;
+	this.time = 0;
 
-    public void    step() { 
-       // Stegar fram klocka ett steg
     }
 
-    public boolean isGreen()   {
+    public int getPeriod(){
+	return this.period;
+
+    }
+    
+    public int getTime(){
+	return this.time;
+    }
+    
+    public int getGreen(){
+	return this.green;
+    }
+    public void step() {
+	int a = this.getTime();
+	this.time = (a + 1);
+    }
+
+    public boolean isGreen()  {
+	if (this.getTime() < this.getGreen()){
+		return true;
+	    }
+	return false;
 	// Returnerar true om time<green, annars false
     }
+   
+    public String  toString()  {
+	return "Light(period = " + this.getPeriod() + " time = " + this.getTime() + " green = " + this.getGreen() + ")";
 
-    public String  toString()  {... }
+}
 	
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,26 @@ public class Main{
     public static void main(String [] args){
 	Car a = new Car(5,0);
 	System.out.println(a.toString());
+	Light b = new Light(10,3);
+	System.out.println(b.toString());
+	if (b.isGreen()){
+	    System.out.println("green!");
+	}
+	else if (!b.isGreen()){
+	    System.out.println("red!");
+}
+	b.step();
+	b.step();
+	b.step();
+	b.step();
 
-
+	if (b.isGreen()){
+	    System.out.println("green!");
+	}
+	else if (!b.isGreen()){
+	    System.out.println("red!");
+}	
+	System.out.println(b.toString());
     }
 
 
